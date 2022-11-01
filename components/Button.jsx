ff2f492e6d006d16c610ff0dac1c7dae5ba5487a
Liliane Iradukunda <Lialsina03@gmail.com>
2022-11-02 01:43:34 +0200

    feat:add hero-section

---
+++ components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ children, className }) => {
+  return (
+    <div>
+      <button
+        className={`outline-none bg-blue border-2 border-blue text-white px-8 py-2 relative overflow-hidden ${className} w-max`}
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
